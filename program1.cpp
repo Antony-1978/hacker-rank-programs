c647428f5df71e24d1c8ced92270fce9ff5d1f9e
Antony-1978 <antonypraveen2246@gmail.com>
2026-04-27 11:08:29 +0530

    Update program1.cpp

--- program1.cpp
+++ program1.cpp
@@ -1,42 +1,55 @@
-#include <bits/stdc++.h>
+#include <iostream>
 
 using namespace std;
-
-string ltrim(const string &);
-string rtrim(const string &);
-
 
 
 int main()
 {
-    string n_temp;
-    getline(cin, n_temp);
-
-    int n = stoi(ltrim(rtrim(n_temp)));
-
-    // Write your code here
-
-    return 0;
+   int x;
+   cin>>x;
+   if(x>9)
+   {
+    cout<<"Greater than 9"<<endl;
+   }
+   else
+   {
+    if(x==1)
+    {
+        cout<<"one"<<endl;
+    }
+    else if(x==2)
+    {
+        cout<<"two";
+    }
+    else if(x==3)
+    {
+        cout<<"three";
+    }
+    else if(x==4)
+    {
+        cout<<"four";
+    }
+    else if(x==5)
+    {
+        cout<<"five";
+        
+    }
+    else if(x==6)
+    {
+        cout<<"six";
+    }
+    else if (x==7)
+    {
+        cout<<"seven";
+    }
+    else if(x==8)
+    {
+        cout<<"eight";
+    }
+    else if(x==9)
+    {
+        cout<<"nine";
+    }
+   }
 }
 
-string ltrim(const string &str) {
-    string s(str);
-
-    s.erase(
-        s.begin(),
-        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
-    );
-
-    return s;
-}
-
-string rtrim(const string &str) {
-    string s(str);
-
-    s.erase(
-        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
-        s.end()
-    );
-
-    return s;
-}
